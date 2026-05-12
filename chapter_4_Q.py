8c678adb948a4244d13d3277c23c228ff216967b
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-05-12 19:21:39 +0530

    just nothing

---
+++ chapter_4_Q.py
@@ -0,0 +1,27 @@
+from functools import reduce
+# lst = (input("enter 6 fruits name : ").split())
+# print(lst)
+
+# marks = list(map(int,input("enter studnt marks :").split()))
+# print(marks)
+
+# marks.sort()
+# print(marks)
+
+
+#using reduce function 
+# sum = list(map(int,input("enter studnt marks :").split()))
+# print(sum)
+
+# list = reduce(lambda x ,y: x+y , sum)
+# print(list)
+
+#counting element in tuple 
+a = (7,0,8,0,0,9)
+new_tpl = a.count(0)
+print(new_tpl)
+
+
+
+
+
