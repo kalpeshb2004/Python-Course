feee31fb474caa35793e935f8cd35e68c9ce5894
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-04 18:42:33 +0530

    string manipulation

---
+++ hackerrank/basic.py/mutableStrChange.py
@@ -0,0 +1,17 @@
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:] 
+
+# 5 tak character alag hua aur hamara char add hua
+# and then same 5 se chalu hua to hamara char insert hua replace nahi
+# ex = abracadabra  / without +1
+# 5 tak count hua abrac
+# add hua k
+# 5 se chalu hua adabra 
+# result aaya =abrackadabra
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
