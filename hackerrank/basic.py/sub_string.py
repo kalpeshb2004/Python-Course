f1e857d5497da73d0d49d491ec43c0ff7bf70420
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-05 16:45:08 +0530

    hard one

--- hackerrank/basic.py/sub_string.py
+++ hackerrank/basic.py/sub_string.py
@@ -1,6 +1,20 @@
 def count_substring(string, sub_string):
-    return string.count(sub_string)
-       
+    count = 0
+    for i in range(0,len(string)-len(sub_string)):
+        if string[i:i+len(sub_string)] == sub_string:
+            count+=1
+
+    return count   
+
+#     0123456
+# A = bababcq = 7
+# bab = 3
+# A[0:3] == bab, count = 1
+# A[1:4] == aba, count = 1
+# A[2:5] == bab, count = 2
+# A[3:6] == abc, count = 2
+# A[4:7] == bcq, count = 2
+#
     
 if __name__ == '__main__':
     string = input().strip()
