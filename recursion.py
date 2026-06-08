bd9dd3b433381fdeee2e0e6a09fd0c1168d18d47
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-08 15:04:50 +0530

    recursion practice

---
+++ recursion.py
@@ -0,0 +1,23 @@
+# #factorial of a number using recursion
+# def factorial(n):
+#     if (n==0 or n==1):
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+# print(factorial(7)) 
+# print(factorial(5))  
+
+# #fibonicci series using recursion 
+# def fibonicci(n):
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         return 1
+#     else:
+#         return fibonicci(n-1) + fibonicci(n-2)
+
+
+# for i in range(10):
+#     print(fibonicci(i), end=" ")    
+    
