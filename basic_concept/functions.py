469a54abf4afa999952b9573c46967d6004e104f
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-12 19:05:46 +0530

    string thoda hard he

--- basic_concept/functions.py
+++ basic_concept/functions.py
@@ -30,3 +30,36 @@
 # print(calculate_div(a,b))
 
 2
+# P2 — palindrome checker
+# function banao is_palindrome(word)
+# word ulta same hai → return True, else False
+
+# def palindrome(stri):
+#     temp = ""
+#     for i in range(0,len(stri)):
+#         temp = stri[i] + temp
+#     return temp
+
+# print(palindrome("hello"))
+
+
+3
+# P3 — marks summary
+# function summary(*marks) banao
+# total, average, highest, lowest return kare
+# call: summary(80, 90, 70, 85, 95)
+
+# def summary(*marks):
+#     total = sum(marks)
+#     average = sum(marks) / len(marks)
+#     highest = max(marks)
+#     lowest = min(marks)
+#     return total , average , highest , lowest
+
+# total,average,highest,lowest = summary(39,70,48,47)
+# print("total ",total)
+# print("average ",average)
+# print("highest ",highest)
+# print("lowest ",lowest)
+
+
