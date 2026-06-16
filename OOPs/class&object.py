0a5286ef3622b06dcafbc1d78f5a99a1978ebd51
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-16 22:39:10 +0530

    first problem simple

---
+++ OOPs/class&object.py
@@ -0,0 +1,17 @@
+# P1 — Easy
+# Student class banao. Attributes: name, age, marks. Method: introduce() jo print kare —
+# "Mera naam Raj hai, age 20, marks 95"
+
+# class student:
+#     def __init__(self,name,age,marks):
+#         self.name = name
+#         self.age = age
+#         self.marks = marks
+    
+#     def introduce(self):
+#         print(f'Mera name {self.name} hai, age {self.age}, marks {self.marks}')
+
+# std1 = student("Raj",20,95)
+
+# std1.introduce()
+
