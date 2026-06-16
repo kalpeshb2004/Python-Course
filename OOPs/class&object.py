d02a31d01a4043af8d6bbdb4e809a196fa5fd68e
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-16 23:21:23 +0530

    last 4th problem done

--- OOPs/class&object.py
+++ OOPs/class&object.py
@@ -43,6 +43,50 @@
 # bal1.deposit(500)
 # bal1.withdraw(200)
 # bal1.show_balance()
+
+# P3 — Medium
+# Rectangle class banao. Attributes: length, width. Methods:
+# area() → length × width
+# perimeter() → 2 × (length + width)
+# is_square() → True/False (length == width)
+
+# P4 — Hard
+
+# Library class banao. Attributes: books (list). Methods:
+# add_book(title) → list me add
+# remove_book(title) → list se hatao
+# search_book(title) → mila toh "Found" nahi toh "Not found"
+# show_all() → sab books print karo
+
+# class Library:
+#     def __init__(self,book):
+#         self.book = []
+
+#     def add_book(self,title):
+#         self.book.append(title)
     
+#     def remove_book(self,title):
+#         self.book.remove(title)
+    
+#     def serach_book(self,title):
+#         if title in self.book:
+#             print("Found")
+#         else:
+#             print("Not Found")
+    
+#     def show_all(self):
+#         print(f'Books: {self.book}')
+
+# lbr = Library("forest")
+
+# lbr.add_book("jungle")
+# lbr.add_book("mangle")
+# lbr.add_book("dangal")
+# lbr.remove_book("dangal")
+# lbr.serach_book("jungle")
+# lbr.show_all()
+
+
+        
 
 
