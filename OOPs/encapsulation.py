ad909eac9fc247d0bcf490851e462a17eda74a5f
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-18 16:56:29 +0530

    Q1 done

---
+++ OOPs/encapsulation.py
@@ -0,0 +1,23 @@
+# P1 — Easy
+# Person class banao — name public, _email protected, __password private. Getter + setter banao __password ke liye. Password change karo setter se.
+
+# class person:
+#     def __init__(self,name,email,password):
+#         self.name = name
+#         self._email = email
+#         self.__password = password
+
+#     def get_pass(self):
+#         return self.__password
+    
+#     def set_pass(self , chng_pass):
+#         if chng_pass == "abc123":
+#             self.__password = "123abc"
+#         else:
+#             print("invalid password")
+            
+# p1 = person("kalpesh","kalpeshbhure01@gmail.com","abc1243")
+# print(p1.get_pass())
+# p1.set_pass("2222")
+# print(p1.get_pass())
+
