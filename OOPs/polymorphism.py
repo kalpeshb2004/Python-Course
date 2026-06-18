9b90037f3c5323635daf9363b05d05ed4f5ed565
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-18 14:10:30 +0530

    Q1 done

---
+++ OOPs/polymorphism.py
@@ -0,0 +1,29 @@
+# P1 — Easy (Overriding)
+# Employee class — work() method → "Employee kaam kar raha hai". 3 child classes: Developer, Designer, Manager. Har ek work() override kare apne kaam ke saath. Loop me sab ka work() bulao.
+
+# class employee:
+#     def work(self):
+#         print("Employee kaam kar raha hai")
+
+# class developer(employee):
+#     def work(self):
+#         print("developer kaam kar raha hai")
+
+# class designer(employee):
+#     def work(self):
+#         print("designer kaam kar raha hai")
+
+# class manager(employee):
+#     def work(self):
+#         print("manager kaam kar raha hai")
+
+# workers = [developer() , designer() , manager()]
+# for w in workers:
+#     w.work()
+
+# P2 — Medium (Overloading)
+# Calculator class — multiply() method jo:
+# 1 number → square kare
+# 2 numbers → multiply kare
+# 3 numbers → teeno multiply kare
+
