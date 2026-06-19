153d8d7c098c0fe70156580405be0bb51c94d4f3
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-06-19 12:59:50 +0530

    Q1 done

---
+++ OOPs/abstraction.py
@@ -0,0 +1,45 @@
+# P1 — Easy
+# Vehicle abstract class banao — @abstractmethod: start(), stop(). 2 child: Car, Bike. Dono me implement karo. Objects banao aur methods bulao.
+
+# from abc import ABC , abstractmethod
+# class Vehicle(ABC):
+#     @abstractmethod
+#     def start(self):
+#         pass
+
+#     @abstractmethod
+#     def stop(self):
+#         pass
+
+# class Car(Vehicle):
+#     def start(self):
+#         print("car is started")
+    
+#     def stop(self):
+#         print("car is stoped")
+    
+# class Bike(Vehicle):
+#     def start(self):
+#         print("Bike is started")
+
+#     def stop(self):
+#         print("Bike is stoped")
+
+# c1 = Car()
+# c1.start()
+# c1.stop()
+
+# b1 = Bike()
+# b1.start()
+# b1.stop()
+
+
+
+
+
+
+
+
+
+
+
