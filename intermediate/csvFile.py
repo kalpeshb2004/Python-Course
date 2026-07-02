f4092804d04a684303a96b6eb93a10951b5a9f9a
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-02 13:14:37 +0530

    csv started

---
+++ intermediate/csvFile.py
@@ -0,0 +1,6 @@
+with open("data_csv.csv","w") as f:
+    f.write("name,age,class\n")
+    f.write("kalpesh,21,MCA\n")
+    f.write("jayesh,25,web_developer\n")
+    f.write("dharmraj,50,contractor")
+
