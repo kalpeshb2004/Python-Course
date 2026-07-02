835e1f6e375f27d1fcfec88150c7722261458da0
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-02 19:31:56 +0530

    Q2 done

--- intermediate/exceptionHandling.py
+++ intermediate/exceptionHandling.py
@@ -13,3 +13,23 @@
 # except ValueError:
 #     print("value must be integer")
 
+# P2 — Medium
+# User se file name lo. File padho aur print karo. Handle karo:
+
+# FileNotFoundError — file nahi mili
+# PermissionError — access nahi
+# finally me print karo "Operation complete"
+
+# filename = input("enter a file name : ")
+
+# try:
+#     with open(filename,"r") as f:
+#         data = f.read()
+#         print(data)
+# except FileNotFoundError:
+#     print("error: filke not found")
+# except PermissionError:
+#     print("error: you dont have a permission to open this file")
+# finally:
+#     print("operation complete")
+
