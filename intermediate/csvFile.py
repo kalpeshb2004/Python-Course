6f82cd384a6fc7608dd286e6dbda4d367570b61f
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-02 13:59:48 +0530

    Q3 has been done

--- intermediate/csvFile.py
+++ intermediate/csvFile.py
@@ -16,3 +16,25 @@ with open("student.csv","r") as f:
     reader = csv.DictReader(f)
     for row in reader:
         print(row)
+
+with open("student.csv","r") as f:
+    marks_lst = []
+    name_lst = []
+    reader = csv.DictReader(f)
+    for row in reader:
+        if int(row["marks"]) > 40:
+            print(row['name'],row['age'],row['marks'])
+        marks_lst.append(int(row["marks"]))
+        high_marks = max(marks_lst)
+        name_lst.append(row["name"])
+        
+
+    average = sum(marks_lst) / len(marks_lst)
+    print(average)
+
+    max_marks = max(marks_lst)
+    index = marks_lst.index(max_marks)
+    print(f"Highest: {name_lst[index]} ({max_marks})")
+    
+    
+   
