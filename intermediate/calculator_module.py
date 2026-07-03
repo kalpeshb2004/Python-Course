7090a0ab545c998b9ee60313056ca3952e8fb42a
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-03 17:35:25 +0530

    Q1 done

---
+++ intermediate/calculator_module.py
@@ -0,0 +1,17 @@
+# P1 — Easy
+# calculator.py module banao — add(), subtract(), multiply(), divide(). main.py me import karke use karo. if __name__ == "__main__" bhi use karo.
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def devide(a,b):
+    return a / b
+
+if __name__ == "__main__":
+    print(add(10,20))
---
+++ intermediate/module_package_PIP.py
@@ -0,0 +1,10 @@
+# P1 — Easy
+# calculator.py module banao — add(), subtract(), multiply(), divide(). main.py me import karke use karo. if __name__ == "__main__" bhi use karo.
+
+import calculator_module
+
+print(calculator_module.add(10,20))
+print(calculator_module.subtract(10,30))
+print(calculator_module.multiply(10,20))
+print(calculator_module.devide(10,20))
+
