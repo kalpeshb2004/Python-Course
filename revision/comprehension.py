a0bc15e09c6e8a117b79838eb402ccdfdd7871b3
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-09 20:13:29 +0530

    problmes on list and dict comprehension

---
+++ revision/comprehension.py
@@ -0,0 +1,14 @@
+# COMPREHENSIONS
+# Q1 — Easy
+# List comprehension se 1-20 ke sirf odd numbers nikalo.
+
+# lst = [x for x in range(0,20) if x%2 != 0]
+# print(lst)
+
+# Q2 — Medium
+# Dict comprehension — words ki list lo, har word ki length dict me store karo. 5 se zyada length wale sirf lo.
+
+# words = ["kalpesh","jayesh","dhjarmraj","ram","rahul","shreyas"]
+# lst = {word: len(word) for word in words if len(word) > 5}
+# print(lst)
+
