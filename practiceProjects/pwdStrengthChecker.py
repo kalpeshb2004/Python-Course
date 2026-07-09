bff74c2a7c54c7b1066b1f542013d6982f406703
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-09 14:06:48 +0530

    with function done

--- practiceProjects/pwdStrengthChecker.py
+++ practiceProjects/pwdStrengthChecker.py
@@ -20,40 +20,47 @@ pwd = input("Enter your password: ")
 score = 0
 feedback = []
 
-if 8 <= len(pwd) <= 15:
-    score += 1
-else:
-    feedback.append("password length should be 8-15")
+def strengthChecker():
+    global score,feedback
+    if 8 <= len(pwd) <= 15:
+     score += 1
+    else:
+        feedback.append("password length should be 8-15")
 
-if any(c.upper() for c in pwd):
-    score += 1
-else:
-    feedback.append("atleast one capital letter")
+    if any(c.isupper() for c in pwd):
+        score += 1
+    else:
+        feedback.append("atleast one capital letter")
 
-if any(c.lower() for c in pwd):
-    score += 1
-else:
-    feedback.append("atleast one small letter")
+    if any(c.islower() for c in pwd):
+        score += 1
+    else:
+        feedback.append("atleast one small letter")
 
-if any(c.isdigit() for c in pwd):
-    score +=1
-else:
-    feedback.append("atleast one number")
+    if any(c.isdigit() for c in pwd):
+        score +=1
+    else:
+        feedback.append("atleast one number")
 
-if any(c in "!@#$%^&*" for c in pwd):
-    score +=1
-else:
-    feedback.append("atleast one special character")
+    if any(c in "!@#$%^&*" for c in pwd):
+        score +=1
+    else:
+        feedback.append("atleast one special character")
 
 #result
-if score == 5:
-    print("Password accpetd \nstrong password")
+def scoreCalculate():
+    if score == 5:
+     print("Password accpetd \nstrong password")
 
-elif score >= 3:
-    print("medium pasword")
-else:
-    print("easy pasword")
+    elif score >= 3:
+        print("medium pasword")
+    else:
+        print("easy pasword")
 
-for f in feedback:
-    print("→", f)
+def result():
+    for f in feedback:
+        print("->",f)
 
+strengthChecker()
+scoreCalculate()
+result()
