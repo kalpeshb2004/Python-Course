6a5afef974907403e8e936db7a5a5af127d37417
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-09 14:01:24 +0530

    password strength checker

---
+++ practiceProjects/pwdStrengthChecker.py
@@ -0,0 +1,59 @@
+# Input
+#   ↓
+# 5 rules check
+#   ↓
+# Score calculate
+#   ↓
+# Weak/Medium/Strong
+#   ↓
+# Feedback print
+'''
+length should be gt=8 lt=15
+atleast ek upper case 
+atlest ek lower case
+ek number
+ek cpacial char
+'''
+
+pwd = input("Enter your password: ")
+
+score = 0
+feedback = []
+
+if 8 <= len(pwd) <= 15:
+    score += 1
+else:
+    feedback.append("password length should be 8-15")
+
+if any(c.upper() for c in pwd):
+    score += 1
+else:
+    feedback.append("atleast one capital letter")
+
+if any(c.lower() for c in pwd):
+    score += 1
+else:
+    feedback.append("atleast one small letter")
+
+if any(c.isdigit() for c in pwd):
+    score +=1
+else:
+    feedback.append("atleast one number")
+
+if any(c in "!@#$%^&*" for c in pwd):
+    score +=1
+else:
+    feedback.append("atleast one special character")
+
+#result
+if score == 5:
+    print("Password accpetd \nstrong password")
+
+elif score >= 3:
+    print("medium pasword")
+else:
+    print("easy pasword")
+
+for f in feedback:
+    print("→", f)
+
