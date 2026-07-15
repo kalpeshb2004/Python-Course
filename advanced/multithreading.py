ee14999cafb559ea5d560b528149e34e4b740adc
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-07-15 19:45:55 +0530

    basic program of multithreading

---
+++ advanced/multithreading.py
@@ -0,0 +1,22 @@
+#basic program of multithreading 
+# import threading
+# import time
+
+# def task(name):
+#     print(f"{name} start")
+#     time.sleep(2)
+#     print(f"{name} done")
+
+# t1 = threading.Thread(target=task, args=("thread-1",))
+# t2 = threading.Thread(target=task, args=("thread-2",))
+
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+# print("all task finished")
+
+
+
