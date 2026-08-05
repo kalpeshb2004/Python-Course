b3059533d572029e78e13d5cfbab38b89009bd09
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-08-05 16:01:29 +0530

    practice project add / search / delete / show

---
+++ practiceProjects/contact_book.py
@@ -0,0 +1,44 @@
+contact = {}
+
+def add_contact():
+    name = input("enter a name: ")
+    number = input("enter a number: ")
+    contact[name] = number
+    print("added!")
+
+
+def search_contact():
+    name = input("enter name to search: ")
+    if name in contact:
+        print(f"{name} : {contact[name]}")
+    else:
+        print("contact not found")
+    
+
+def delete_contact():
+    name = input("enter name to delete: ")
+    if name in contact:
+        del contact[name]
+        print("contact deleted")
+    else:
+        print("contact not found")
+
+while True:
+    command = input("enter add/serach/delete/show: ").lower()
+
+    if command == "add":
+        add_contact()
+    elif command == "search":
+        search_contact()
+    elif command == "delete":
+        delete_contact()
+    elif command == "show":
+        print(contact)
+    elif command == "exit":
+        print("bye")
+        break
+    else:
+        print("invalid input!")
+
+   
+
