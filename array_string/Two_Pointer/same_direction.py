eb3027309c2fb5a9425d1ecb313b8a60432cc3d2
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-09-09 18:53:04 +0530

    intersection of two array done

--- array_string/Two_Pointer/same_direction.py
+++ array_string/Two_Pointer/same_direction.py
@@ -137,5 +137,67 @@ def merge_two_sorted_array(arr1,arr2):
 
 print(merge_two_sorted_array([1,1,1,2,4,6,7],[1,2,3,6,7,8,9,10]))
 
+# **6. Union of Two Sorted Arrays (no duplicates)**
+
+# Input: `arr1 = [1, 3, 4, 5]`, `arr2 = [2, 3, 5, 6]`
+
+# Output: `[1, 2, 3, 4, 5, 6]`
+
+def union_array(arr1,arr2):
+    i = 0
+    j = 0
+    result = []
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:
+            if len(result) == 0 or arr1[i] != result[-1]:
+                result.append(arr1[i])
+            i += 1
+        else:
+            if len(result) == 0 or arr2[j] != result[-1]:
+                result.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        if len(result) == 0 or arr1[i] != result[-1]:
+            result.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        if len(result) == 0 or arr2[j] != result[-1]:
+            result.append(arr2[j])
+        j += 1
+
+    return result
+
+print(union_array([1, 3, 4, 5],[2, 3, 5, 6]))
+
+# **7. Intersection of Two Sorted Arrays**
+
+# Input: `arr1 = [1, 2, 4, 5, 6]`, 
+#         `arr2 = [2, 3, 5, 7]`
+
+# Output: `[2, 5]`
+
+def intersection_array(arr1,arr2):
+    i = 0
+    j = 0
+    result = []
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] == arr2[j]:
+            if len(result) == 0 or result[-1] != arr1[i]:
+                result.append(arr1[i])
+            i += 1
+            j += 1
+        elif arr1[i] < arr2[j]:
+            i += 1
+        else:
+            j += 1
+
+    return result
+
+print(intersection_array([1, 2, 4, 5, 6],[2, 3, 5, 7]))
+    
 
 
