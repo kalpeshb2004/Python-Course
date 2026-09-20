e938864622feb310dbdcc7a167972fbf1663fcd5
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-09-20 20:58:28 +0530

    oppsite/end work done

--- array_string/Two_Pointer/opp_end.py
+++ array_string/Two_Pointer/opp_end.py
@@ -4,37 +4,37 @@
 # Input: "hello"
 # Expected Output: "olleh"
 
-# def reverse(s):
-#     s = list(s)
-#     left = 0
-#     right = len(s) - 1
+def reverse(s):
+    s = list(s)
+    left = 0
+    right = len(s) - 1
 
-#     while left < right:
-#         s[left] , s[right] = s[right] , s[left]
-#         left += 1
-#         right -= 1
+    while left < right:
+        s[left] , s[right] = s[right] , s[left]
+        left += 1
+        right -= 1
 
-#     return "".join(s)
+    return "".join(s)
 
-# print(reverse("hello"))
+print(reverse("hello"))
 
 2
 # 2. Reverse an array
 # Input: [1, 2, 3, 4, 5]
 # Expected Output: [5, 4, 3, 2, 1]
 
-# def array(arr):
-#     left = 0
-#     right = len(arr) - 1
+def array(arr):
+    left = 0
+    right = len(arr) - 1
 
-#     while left < right:
-#         arr[left] , arr[right] = arr[right] , arr[left]
-#         left += 1
-#         right -= 1
+    while left < right:
+        arr[left] , arr[right] = arr[right] , arr[left]
+        left += 1
+        right -= 1
 
-#     return arr
+    return arr
 
-# print(array([1,2,3,4,5]))
+print(array([1,2,3,4,5]))
 
 # # 3. Check palindrome (string)
 # # Input: "madam"
@@ -42,19 +42,19 @@
 # # Input 2: "hello"
 # # Expected Output: False
 
-# def  palindrome(s):
-#     s = list(s)
-#     left = 0
-#     right = len(s) - 1
+def  palindrome(s):
+    s = list(s)
+    left = 0
+    right = len(s) - 1
 
-#     while left < right:
-#         if s[left] != s[right]:
-#             return False
-#         left += 1
-#         right -= 1
-#     return True
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
 
-# print(palindrome("madam"))
+print(palindrome("madam"))
 
 # 4. Valid palindrome (ignore spaces/punctuation, case)
 # Input: "A man, a plan, a canal: Panama"
@@ -62,23 +62,23 @@
 # Input 2: "race a car"
 # Expected Output: False
 
-# def valid_palindrome(s):
-#     left = 0
-#     right = len(s) - 1
+def valid_palindrome(s):
+    left = 0
+    right = len(s) - 1
 
-#     while left < right:
-#         while left < right and not s[left].isalnum():
-#             left += 1
-#         while left < right and not s[right].isalnum():
-#             right -= 1
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
 
-#         if s[left].lower() != s[right].lower():
-#             return False
+        if s[left].lower() != s[right].lower():
+            return False
 
-#         left += 1
-#         right -= 1
+        left += 1
+        right -= 1
 
-#     return True
+    return True
 
 # print(valid_palindrome("A man, a plan, a canal: Panama"))
 
@@ -86,22 +86,22 @@
 # Input: arr = [1, 2, 3, 4, 6], target = 6
 # Expected Output: (1, 3) (index) ya (2, 4) (values) — pair jinka sum 6 ho     
 
-# arr = [1,2,3,4,6]
-# target = 6
+arr = [1,2,3,4,6]
+target = 6
 
-# left = 0
-# right = len(arr) - 1
+left = 0
+right = len(arr) - 1
 
-# while left < right:
-#     total = arr[left] + arr[right]
+while left < right:
+    total = arr[left] + arr[right]
 
-#     if total == target:
-#         print(arr[left] , arr[right])
-#         break
-#     elif total < target:
-#         left += 1
-#     elif total > target:
-#         right -= 1
+    if total == target:
+        print(arr[left] , arr[right])
+        break
+    elif total < target:
+        left += 1
+    elif total > target:
+        right -= 1
     
 # 6. Reverse vowels of a string
 # Input: "hello"
@@ -109,26 +109,26 @@
 # Input 2: "leetcode"
 # Expected Output: "leotcede" 
 
-# def vowels_swpping(s):
-#     s = list(s)
-#     left = 0
-#     right = len(s) - 1
+def vowels_swpping(s):
+    s = list(s)
+    left = 0
+    right = len(s) - 1
 
-#     while left < right:
-#         while left < right and s[left] not in "aeiou":
-#             left += 1
-#         while left < right and s[right] not in "aeiou":
-#             right -= 1
+    while left < right:
+        while left < right and s[left] not in "aeiou":
+            left += 1
+        while left < right and s[right] not in "aeiou":
+            right -= 1
 
-#         s[left] , s[right] = s[right] , s[left]
+        s[left] , s[right] = s[right] , s[left]
 
-#         left += 1
-#         right -= 1
+        left += 1
+        right -= 1
 
-#     return "".join(s)
+    return "".join(s)
 
-# print(vowels_swpping("hello"))
-# print(vowels_swpping("leetcode"))
+print(vowels_swpping("hello"))
+print(vowels_swpping("leetcode"))
 
 # 7. Check if string is palindrome after removing at most 1 character
 # Input: "abca"
@@ -136,57 +136,57 @@
 # Input 2: "abc"
 # Expected Output: False
 
-# def palindrome(s):
-#     s = list(s)
-#     left = 0
-#     right = len(s) - 1
+def palindrome(s):
+    s = list(s)
+    left = 0
+    right = len(s) - 1
 
-#     while left < right:
-#        if s[left] != s[right]:
-#            temp = s.copy()
-#            temp.pop(left)
+    while left < right:
+       if s[left] != s[right]:
+           temp = s.copy()
+           temp.pop(left)
 
-#            if temp == temp[::-1]:
-#               return True
+           if temp == temp[::-1]:
+              return True
 
-#            temp = s.copy()
-#            temp.pop(right)
+           temp = s.copy()
+           temp.pop(right)
 
-#            if temp == temp[::-1]:
-#                return True
+           if temp == temp[::-1]:
+               return True
 
-#            return False
-#        left += 1
-#        right -= 1
+           return False
+       left += 1
+       right -= 1
 
-#     return True
+    return True
 
-# print(palindrome("abca"))
+print(palindrome("abca"))
 
 # 8. Reverse a string maintaining word order *******
 # Input: "I love Java"
 # Output: "I evol avaJ"
 # (Har word ke letters reverse, but word order same)
 
-# def reverse_maintain_order(s):
-#     words = s.split(" ")
-#     result = []
+def reverse_maintain_order(s):
+    words = s.split(" ")
+    result = []
 
-#     for word in words:
-#         word = list(word)
-#         left = 0
-#         right = len(word) - 1
+    for word in words:
+        word = list(word)
+        left = 0
+        right = len(word) - 1
 
-#         while left < right:
-#             word[left] , word[right] = word[right] , word[left]
-#             left += 1
-#             right -= 1
+        while left < right:
+            word[left] , word[right] = word[right] , word[left]
+            left += 1
+            right -= 1
 
-#         result.append("".join(word))
+        result.append("".join(word))
 
-#     return " ".join(result)
+    return " ".join(result)
 
-# print(reverse_maintain_order("I Love Java"))
+print(reverse_maintain_order("I Love Java"))
 
 # 9. Compare Version Numbers
 # Input: version1 = "1.2", version2 = "1.10"
@@ -194,26 +194,26 @@
 # Input 2: version1 = "1.01", version2 = "1.001"
 # Output: 0
 
-# def compare_version(version1,version2):
-#     parts1 = version1.split(".")
-#     parts2 = version2.split(".")
+def compare_version(version1,version2):
+    parts1 = version1.split(".")
+    parts2 = version2.split(".")
 
-#     max_len = max(len(parts1), len(parts2))
+    max_len = max(len(parts1), len(parts2))
 
-#     i = 0
+    i = 0
 
-#     while i < max_len:
-#         num1 = int(parts1[i]) if i < len(parts1) else 0
-#         num2 = int(parts2[i]) if i < len(parts2) else 0
+    while i < max_len:
+        num1 = int(parts1[i]) if i < len(parts1) else 0
+        num2 = int(parts2[i]) if i < len(parts2) else 0
 
-#         if num1 < num2:
-#             return -1
-#         if num1 > num2:
-#             return 1
+        if num1 < num2:
+            return -1
+        if num1 > num2:
+            return 1
 
-#         i += 1
+        i += 1
 
-#     return 0
+    return 0
 
 # print(compare_version("1.2", "1.10"))     # -1
 # print(compare_version("1.01", "1.001"))
@@ -357,7 +357,6 @@ def square_array(arr):
     left = 0
     right = len(arr) - 1
     result = []
-    pos = 0
 
     while left <= right:
         left_sq = arr[left] ** 2
